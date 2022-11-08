4b846c4b68162ad7824d6399c038cccefdd04513
Caio Silva <caiopacheco02@gmail.com>
2022-11-07 22:17:47 -0300

    0

---
+++ ProjetoArquivoC#/Arquivos/CPF.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.IO;
+using Arquivos;
+
+namespace CPF
+{
+    public class CPF
+    {
+        public static bool temCPF(bool CPFF)
+        {
+
+            try
+            {   
+                Console.WriteLine("Cliente é pessoa física? Digite true ou false. ");
+                CPFF=Convert.ToBoolean(Console.ReadLine());
+                Program.eCPF=CPFF;
+                return CPFF;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Erro. Digite true ou false.");
+                CPFF=false;
+                temCPF(CPFF);
+                return CPFF;
+                
+            }
+
+        }
+        public static long CPFnumero(bool CPFF1, long CPFFnumeracao=0)
+        {
+            try
+            {
+                if (CPFF1==true)
+
+                {   
+                    Console.WriteLine("Digite o CPF");
+                    CPFFnumeracao=Convert.ToInt64(Console.ReadLine());
+
+                }else
+                {
+                    Console.WriteLine("Digite o CPNJ");
+                    CPFFnumeracao=Convert.ToInt64(Console.ReadLine());
+                }
+            
+            Program.eCPFnumber=CPFFnumeracao;
+
+            return CPFFnumeracao;
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Erro. Digite um número válido.");
+                CPFFnumeracao=0;
+                CPFnumero(CPFF1);
+                return CPFFnumeracao;
+            }
+        }
+        
+    }
+
+} 
+
---
+++ ProjetoArquivoC#/Arquivos/Program.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.IO;
+using CPF;
+
+namespace Arquivos
+{
+    public class Program
+    {
+
+        public static bool eCPF {get; set;}
+        public static string eCPFtype{get; set;}="";
+
+        private static long _eCPFnumber;
+        public static long eCPFnumber
+        {
+            get {return eCPFnumber;}
+            set {_eCPFnumber=value;}
+        }
+        public static string nome{get; set;}="";
+
+        public static async Task Main(string[] args)
+        {
+
+            List<string> lines=new List<string>();
+            List<string> texto=new List<string>();            
+            var nome="";
+
+            for (var i=0; i<10; i++)
+            {
+                {
+                    CPF.CPF.temCPF(eCPF);
+                    CPF.CPF.CPFnumero(eCPF);
+    
+                    if (eCPF==true)
+                    {
+                        eCPFtype="CPF";
+                    }else
+                    {
+                        eCPFtype="CPNJ";
+                    }
+
+                    Console.WriteLine("Digite o nome do cliente...");
+                    lines.Add(Convert.ToString(nome = (Console.ReadLine())+", Tipo de pessoa: "+eCPFtype+", Numeração: "+_eCPFnumber));
+                    Console.WriteLine("Cliente adicionado!!! Nome: "+lines[i]);
+
+                    await File.WriteAllLinesAsync("arquivo.txt", lines);
+                }       
+            }
+        }
+    
+    }
+}    
+
